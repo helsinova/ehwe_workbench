0e4f83beaf1f1b29dc9ece74e97a5bc449e18294
Michael Ambrus <michael.ambrus@gmail.com>
2016-03-16 22:01:37 +0100

    Testing the new API:s
    These are mostly not map-able to stdperf but needed for performance cases.

--- spiram.c
+++ spiram.c
@@ -24,36 +24,97 @@
 #include <string.h>
 #include <stm32f10x.h>
 #include "spiram.h"
+#include <arpa/inet.h>
 
 #define DEFLT_REPETITIONS 100
+#define DEFLT_START_ADDRESS 0
 #define DEFLT_PATTERN     "Donald Duck .."
+#define SPIx SPI1
+#define SPIRAM_WR_CMD 0x02
+#define SPIRAM_RD_CMD 0x03
 
-void write_spiram(char *pattern, int sz, int times)
+#ifdef HAS_PRINTF
+#define PRINTF printf
+#else
+#define PRINTF(...) ((void)(0))
+#endif
+
+void write_spiram(int address, const char *pattern, int sz, int times)
 {
-    int i, j;
+    int i, j, offset = 0;
+    uint32_t naddress;
+    uint8_t cmd[4];
 
-    while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET) ;
+    cmd[0] = SPIRAM_WR_CMD;
+    naddress = htonl(address);
+    cmd[1] = ((uint8_t *)(&naddress))[1];
+    cmd[2] = ((uint8_t *)(&naddress))[2];
+    cmd[3] = ((uint8_t *)(&naddress))[3];
+
+    while (SPI_I2S_GetFlagStatus(SPIx, SPI_I2S_FLAG_TXE) == RESET) ;
+    PRINTF("SPI-RAM: Write {0x%02X,0x%02X,0x%02X,0x%02X}\n", cmd[0], cmd[1],
+           cmd[2], cmd[3]);
 
     for (i = 0; i < times; i++) {
+        SPI_I2S_SetCS(SPIx, 0);
+        SPI_I2S_SendDataArray_ncs(SPIx, cmd, 4);
         for (j = 0; j < sz; j++) {
-            SPI_I2S_SendData(SPI1, pattern[j]);
+            SPI_I2S_SendData_ncs(SPIx, pattern[j]);
         }
+        SPI_I2S_SetCS(SPIx, 1);
+        PRINTF("%08X: %s\n", address + offset, pattern);
+        offset += sz;
+    }
+}
+
+void read_spiram(int address, char *pattern, int sz, int times)
+{
+    int i, j, offset = 0;
+    uint32_t naddress;
+    uint8_t cmd[4];
+
+    cmd[0] = SPIRAM_RD_CMD;
+    naddress = htonl(address);
+    cmd[1] = ((uint8_t *)(&naddress))[1];
+    cmd[2] = ((uint8_t *)(&naddress))[2];
+    cmd[3] = ((uint8_t *)(&naddress))[3];
+
+    while (SPI_I2S_GetFlagStatus(SPIx, SPI_I2S_FLAG_TXE) == RESET) ;
+
+    PRINTF("SPI-RAM: Read {0x%02X,0x%02X,0x%02X,0x%02X}\n", cmd[0], cmd[1],
+           cmd[2], cmd[3]);
+    for (i = 0; i < times; i++) {
+
+        SPI_I2S_SetCS(SPIx, 0);
+        SPI_I2S_SendDataArray_ncs(SPIx, cmd, 4);
+        for (j = 0; j < sz; j++) {
+            pattern[j] = SPI_I2S_ReceiveData_ncs(SPIx);
+        }
+        SPI_I2S_SetCS(SPIx, 1);
+        PRINTF("%08X: %s\n", address + offset, pattern);
+        offset += sz;
     }
 }
 
 int main(int argc, char **argv)
 {
     int repetitions = DEFLT_REPETITIONS;
+    int startaddress = DEFLT_START_ADDRESS;
     char *pattern = DEFLT_PATTERN;
     int pattsz = sizeof(DEFLT_PATTERN);
 #ifdef STDLIB_TARGET
     int c;
+    uint8_t creg;
+    char *r_pattern;
 
     opterr = 0;
-    while ((c = getopt(argc, argv, "x:")) != -1)
+    while ((c = getopt(argc, argv, "x:a:")) != -1)
         switch (c) {
             case 'x':
                 repetitions = atoi(optarg);
+                break;
+            case 'a':
+                startaddress = atoi(optarg);
                 break;
             case '?':
                 if (optopt == 'x')
@@ -73,10 +134,76 @@ int main(int argc, char **argv)
         pattern = argv[optind];
         pattsz = strlen(pattern);
     }
-
-    printf("SPI-RAM will be filled with test-pattern [%s] %d number of times\n",
-           pattern, repetitions);
 #endif
-    write_spiram(pattern, pattsz, repetitions);
+#ifdef TEST_CS
+    {
+        int i = 0;
+        for (i = 0; i < 1000; i++) {
+            SPI_I2S_SetCS(SPIx, 1);
+            usleep(1000);
+            SPI_I2S_SetCS(SPIx, 0);
+            usleep(10000);
+        }
+    }
+#endif
+
+    r_pattern = strdup(pattern);    /*Just allocate the same size */
+
+#ifdef ONEBYTE
+    PRINTF("SPI-RAM: Read from config register\n");
+    SPI_I2S_SendReceiveData(SPIx, (uint8_t[]) {
+                            0x05}, 1, &creg, 1);
+    PRINTF("SPI-RAM: Config register is: 0x%02X\n", creg);
+
+    PRINTF("SPI-RAM: Write 0x41 to config register\n");
+    SPI_I2S_SendDataArray(SPIx, (uint8_t[]) {
+                          0x01, 0x41}, 2);
+
+    PRINTF("SPI-RAM: Read from config register\n");
+    SPI_I2S_SendReceiveData(SPIx, (uint8_t[]) {
+                            0x05}, 1, &creg, 1);
+    PRINTF("SPI-RAM: Config register is: 0x%02X\n", creg);
+#else
+    PRINTF("SPI-RAM_2: Read from config register\n");
+    SPI_I2S_SetCS(SPIx, 0);
+    SPI_I2S_SendDataArray_ncs(SPIx, (uint8_t[]) {
+                              0x05}, 1);
+    SPI_I2S_ReceiveDataArray_ncs(SPIx, &creg, 1);
+    SPI_I2S_SetCS(SPIx, 1);
+    PRINTF("SPI-RAM_2: Config register is: 0x%02X\n", creg);
+
+    PRINTF("SPI-RAM_2: Write 0x41 to config register\n");
+    SPI_I2S_SetCS(SPIx, 0);
+/*
+    SPI_I2S_SendDataArray_ncs(SPIx, (uint8_t[]) {
+                              0x01}, 1);
+    SPI_I2S_SendDataArray_ncs(SPIx, (uint8_t[]) {
+                              0x41}, 1);
+*/
+    SPI_I2S_SendDataArray_ncs(SPIx, (uint8_t[]) {
+                              0x01, 0x41}, 2);
+    SPI_I2S_SetCS(SPIx, 1);
+
+    PRINTF("SPI-RAM_2: Read from config register\n");
+    SPI_I2S_SetCS(SPIx, 0);
+    SPI_I2S_SendDataArray_ncs(SPIx, (uint8_t[]) {
+                              0x05}, 1);
+    SPI_I2S_ReceiveDataArray_ncs(SPIx, &creg, 1);
+    SPI_I2S_SetCS(SPIx, 1);
+    PRINTF("SPI-RAM_2: Config register is: 0x%02X\n", creg);
+#endif
+
+    PRINTF
+        ("SPI-RAM: will be filled with test-pattern [%s] %d number of times\n",
+         pattern, repetitions);
+    write_spiram(startaddress, pattern, pattsz, repetitions);
+
+    PRINTF
+        ("SPI-RAM: Read test-pattern [%s] %d number of times\n",
+         pattern, repetitions);
+    memset(r_pattern, 0, pattsz);
+    read_spiram(startaddress, r_pattern, pattsz, repetitions);
+
+    free(r_pattern);
     return 0;
 }
